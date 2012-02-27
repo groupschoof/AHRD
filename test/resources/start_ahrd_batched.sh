7f4de10cd1b1f3e832cf380544d228490cd223a6
Asis Hallab <ah@mbp3.local>
2012-02-27 18:37:14 +0100

    Added explanation and example call for Batcher.

---
+++ test/resources/start_ahrd_batched.sh
@@ -0,0 +1,3 @@
+java -Xmx2048m -jar ./dist/ahrd.jar ./test/resources/batch_ymls/batch001.yml
+java -Xmx2048m -jar ./dist/ahrd.jar ./test/resources/batch_ymls/batch002.yml
+java -Xmx2048m -jar ./dist/ahrd.jar ./test/resources/batch_ymls/batch003.yml
