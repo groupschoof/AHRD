c78a0b947be631034400c44ca59b838babd29d25
Asis Hallab <hallab@mpipz.mpg.de>
2012-09-26 12:59:46 +0200

    Writing thorough tests for domain architecture similarity run of AHRD.

--- test/ahrd/test/RunAhrdWithDomainArchitectureTest.java
+++ test/ahrd/test/RunAhrdWithDomainArchitectureTest.java
@@ -1,5 +1,6 @@
 package ahrd.test;
 
+import static ahrd.controller.Settings.getSettings;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -10,7 +11,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 import ahrd.controller.AHRD;
+import ahrd.model.BlastResult;
 import ahrd.model.InterproResult;
+import ahrd.model.Protein;
 
 public class RunAhrdWithDomainArchitectureTest {
 
@@ -30,6 +33,11 @@ public class RunAhrdWithDomainArchitectureTest {
 	 */
 	@Test
 	public void testRunInterpro() throws Exception {
+
+		// In this TEST BLOCK ignore the domain architecture similarity score as
+		// a summand for the token score:
+		getSettings().setTokenScoreDomainSimilarityWeight(0.0);
+
 		// Check correct initialization:
 		ahrd.setup(false);
 
@@ -80,6 +88,32 @@ public class RunAhrdWithDomainArchitectureTest {
 		assertNotNull(
 				"InterPro Entry 'IPR000001' has no Domain Weight assigned!", dw);
 		assertEquals(0.189433136086726, dw, 0.0);
+
+		// Test AHRD Scores:
+		ahrd.assignHumanReadableDescriptions();
+
+		// With InterPro domain annotations:
+		Protein p1 = ahrd.getProteins().get("gene:chr01.502:mRNA:chr01.502");
+		BlastResult bestBr1 = p1.getDescriptionScoreCalculator()
+				.getHighestScoringBlastResult();
+		Double descScore1 = bestBr1.getDescriptionScore();
+		assertNotNull(
+				"Description Score of Blast Hit 'sp|Q3EBC8|DCL2_ARATH' should not be NULL!",
+				descScore1);
+		assertNotNull(
+				"Protein 'gene:chr01.502:mRNA:chr01.502' should have a vector in domain architecture space.",
+				p1.getDomainWeights());
+		assertNotNull(
+				"BlastResult 'sp|Q3EBC8|DCL2_ARATH' should have a vector in domain architecture space.",
+				bestBr1.getDomainWeights());
+		assertNotNull(
+				"BlastResult 'sp|Q3EBC8|DCL2_ARATH' should have a computed Domain Architecture Similarity Score.",
+				bestBr1.getDomainSimilarityScore());
+		// But no domains are shared with the best BlastHit, so:
+		assertEquals(0.0, bestBr1.getDomainSimilarityScore(), 0.0);
+		// Expect Description Score to be equal to score calculated as without
+		// domain annotations:
+		assertEquals(2.947, bestBr1.getDescriptionScore(), 0.001);
 	}
 
 }
