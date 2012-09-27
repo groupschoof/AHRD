39b69c6e81bc864113f2700f4fe9b5d27aefd310
Group Prof. Dr. Heiko Schoof <bangalore@mpipz.mpg.de>
2012-09-27 14:18:13 +0200

    Checking for method parseDomainWeights for errors

--- test/ahrd/test/InterproResultTest.java
+++ test/ahrd/test/InterproResultTest.java
@@ -28,7 +28,7 @@ public class InterproResultTest {
 		TestUtils.initTestSettings();
 		InterproResult.initialiseInterproDb();
 		getSettings().setPathToDomainWeightsDatabase(
-				"./test/resources/domain_weights_database_pfam.txt");
+				"./test/resources/domain_weights_database_interpro_test.txt");
 	}
 
 	@Test
@@ -159,7 +159,6 @@ public class InterproResultTest {
 		// Because the above @Before has initialized the Settings and the memory
 		// Interpro-Database, we have it here, already.
 		System.out.println("1");
-		System.out.println(getSettings().getPathToDomainWeightsDatabase());
 		InterproResult.parseDomainWeights();
 		System.out.println("2");
 		// Assure that InterproDomains IPR000535 IPR000536 IPR000006 have their
