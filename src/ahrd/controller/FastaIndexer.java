f972a5a696877b2ff4101fe0cb1d00767d2b1409
Asis Hallab <hallab@mpipz.mpg.de>
2014-12-22 22:37:50 +0100

    Trying out line-wise writing of properties…

--- src/ahrd/controller/FastaIndexer.java
+++ src/ahrd/controller/FastaIndexer.java
@@ -2,51 +2,41 @@ package ahrd.controller;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.io.PrintWriter;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
-import ahrd.model.ReferenceDescription;
+import java.util.Properties;
 
 public class FastaIndexer {
 
-	public static Map<String, String> references = new HashMap<String, String>();
 	public static long timestamp;
 
-	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws Exception {
 		timestamp = (new Date()).getTime();
 		System.out.println("Start");
 		BufferedReader fastaIn = new BufferedReader(new FileReader(args[0]));
 		String str;
+		PrintWriter pw = new PrintWriter(new FileWriter(args[1]));
 		while ((str = fastaIn.readLine()) != null) {
 			if (str.startsWith(">")) {
-				ReferenceDescription rd = ReferenceDescription
-						.constructFromFastaEntry(str.trim());
-				references.put(rd.getAccession(), rd.getDescription());
+				pw.println(str.trim().replaceFirst(">", "")
+						.replaceFirst(" ", "="));
 			}
 		}
 		fastaIn.close();
-		ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(
-				args[1]));
-		out.writeObject(references);
-		out.close();
+		pw.close();
 		System.out.println("Serialized in " + takeTime());
 
-		references = new HashMap<String, String>();
-		ObjectInputStream in = new ObjectInputStream(new FileInputStream(
-				args[1]));
-		references = (Map<String, String>) in.readObject();
-		in.close();
+		Properties prop = new Properties();
+		InputStream stream = new FileInputStream(args[1]);
+		prop.load(stream);
 		System.out.println("Deserialized in " + takeTime());
-		
-		//System.out.println(references);
+
+		// System.out.println(references);
 	}
-	
+
 	static protected long takeTime() {
 		// Measure time:
 		long now = (new Date()).getTime();
