728ca91a043462d16d03e253f29e34cff23c2cd5
Dr. Asis Hallab <hallab@mpipz.mpg.de>
2016-09-09 17:36:09 +0200

    Introducing new AHRD version with Berkeley-Db

--- src/ahrd/controller/AHRD.java
+++ src/ahrd/controller/AHRD.java
@@ -29,7 +29,7 @@ import nu.xom.ParsingException;
 
 public class AHRD {
 
-	public static final String VERSION = "3.11";
+	public static final String VERSION = "3.4";
 
 	private Map<String, Protein> proteins;
 	private Map<String, Double> descriptionScoreBitScoreWeights = new HashMap<String, Double>();
